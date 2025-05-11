704324a577addb5f40c3e589364ca9d744d14024
ELLIS-DELY1 <nexauragency@gmail.com>
2025-05-11 17:32:47 +0200

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  darkMode: 'class', // Enable dark mode via a class
+  content: [
+    './src/**/*.{html,js,jsx,ts,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
